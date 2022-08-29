526cd996721a28809d2664651423c84b22454b07
Anton Oellerer <a.oellerer@docu-tools.com>
2022-08-29 11:19:06 +0200

    Fix pylint problems
    Use separate context for file returned by `_download_file`

--- src/metadata_tagger/object_tagger.py
+++ src/metadata_tagger/object_tagger.py
@@ -3,9 +3,10 @@ The object tagger is responsible for checking whether a file
 has to be tagged, fetching it, and passing it to a supplied tagging
 function
 """
+from contextlib import contextmanager
 import tempfile
 import logging
-from typing import IO, Dict, Callable
+from typing import Dict, Callable, Generator
 
 import boto3
 from botocore.exceptions import ClientError
@@ -14,7 +15,8 @@ from mypy_boto3_s3.type_defs import HeadObjectOutputTypeDef
 s3_client = boto3.client('s3')  # type: ignore
 
 
-def tag_file(key: str, bucket: str, extraction_function: Callable[[str], Dict[str, str]]) -> Dict[str, str]:
+def tag_file(key: str, bucket: str,
+             extraction_function: Callable[[str], Dict[str, str]]) -> Dict[str, str]:
     """
     Returns a dictionary containing the metadata extracted by the passed `extraction_function`
     from the file found in the passed `bucket` with the passed `key`.
@@ -27,15 +29,14 @@ def tag_file(key: str, bucket: str, extraction_function: Callable[[str], Dict[st
         if _already_tagged(object_info):
             logging.info("%s in %s is already tagged", key, bucket)
             return object_info["Metadata"]
-        file = _download_file(key, bucket)  # todo open with
-        custom_tags = extraction_function(file.name)
-        metadata_tags = object_info["Metadata"] | custom_tags | {
-            'docu-tools-tags': "v1"}
-        result = _tag_remote_file(
-            key, bucket, metadata_tags, object_info["ETag"])
-        retries = retries + 1
-        _cleanup(file)
-        return custom_tags
+        with _download_file(key, bucket) as downloaded_file:
+            custom_tags = extraction_function(downloaded_file.name)
+            metadata_tags = object_info["Metadata"] | custom_tags | {
+                'docu-tools-tags': "v1"}
+            result = _tag_remote_file(
+                key, bucket, metadata_tags, object_info["ETag"])
+            retries = retries + 1
+            return custom_tags
     raise RuntimeError("Could not tag file succesfully")
 
 
@@ -43,11 +44,12 @@ def _get_object_info(key: str, bucket: str) -> HeadObjectOutputTypeDef:
     return s3_client.head_object(Bucket=bucket, Key=key)
 
 
-def _download_file(key: str, bucket: str) -> IO[bytes]:
+@contextmanager
+def _download_file(key: str, bucket: str) -> Generator:
     with tempfile.NamedTemporaryFile() as target:
         s3_client.download_file(bucket, key, target.name)
-        logging.debug("Downloaded %s from %s",key, bucket)
-        return target
+        logging.debug("Downloaded %s from %s", key, bucket)
+        yield target
 
 
 def _tag_remote_file(key: str, bucket: str, metadata_tags: Dict[str, str], etag: str):
@@ -63,10 +65,6 @@ def _tag_remote_file(key: str, bucket: str, metadata_tags: Dict[str, str], etag:
     except ClientError as error:
         logging.error(error)
         return False
-
-
-def _cleanup(file: IO[bytes]):
-    file.close()
 
 
 def _already_tagged(object_info: HeadObjectOutputTypeDef) -> bool:
